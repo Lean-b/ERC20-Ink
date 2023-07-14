6c8cc2bdd99b325518874527ad16e09fd57a5da8
Lean-b <leandroborqueza@gmail.com>
2023-07-14 15:01:22 -0300

    Token ERC20

--- lib.rs
+++ lib.rs
@@ -1,10 +1,16 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+
+/*
+    Smart contract de un token ERC20
+*/
+
 #[ink::contract]
 mod erc20 {
 
     use ink::storage::Mapping;
 
+    //Estructura basica de ERC20
     #[ink(storage)]
     #[derive(Default)]
     pub struct Erc20 {
@@ -15,9 +21,10 @@ mod erc20 {
         allowances: Mapping<(AccountId, AccountId), Balance>,
     }
 
+
+    //Evento de transferencias
     #[ink(event)]
     pub struct Transfer {
-        
         #[ink(topic)]
         from: Option<AccountId>,
         #[ink(topic)]
@@ -25,6 +32,7 @@ mod erc20 {
         value: Balance,
     }
 
+    //Evento de aprobacion
     #[ink(event)]
     pub struct Approval {
         #[ink(topic)]
@@ -34,6 +42,8 @@ mod erc20 {
         value: Balance,
     }
 
+
+    //Representar errores basicos
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
@@ -43,7 +53,10 @@ mod erc20 {
 
     pub type Result<T> = core::result::Result<T, Error>;
 
+    //Implementaciones de las funciones basicas del smart contract token ERC20
     impl Erc20 {
+
+         // Constructor del contrato (Se puede agregar mas de un constructor)
         #[ink(constructor)]
         pub fn new(total_supply: Balance) -> Self {
             let mut balances = Mapping::default();
@@ -64,6 +77,7 @@ mod erc20 {
             }
         }
 
+        // Obtiene el saldo de tokens de una cuenta
         #[ink(message)]
         pub fn balance_of(&self, owner: AccountId) -> Balance {
             self.balance_of_impl(owner)
@@ -74,6 +88,7 @@ mod erc20 {
             self.balances.get(owner).unwrap_or_default()
         }
 
+        // Obtiene la asignación de gasto aprobada de una cuenta a otra cuenta
         #[ink(message)]
         pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
             self.allowance_impl(&owner, &spender)
@@ -84,17 +99,20 @@ mod erc20 {
             self.allowances.get((owner, spender)).unwrap_or_default()
         }
 
+        // Obtiene el suministro total de tokens
         #[ink(message)]
         pub fn total_supply(&self) -> Balance {
             self.total_supply
         }
 
+        // Transfiere tokens de una cuenta a otra
         #[ink(message)]
         pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
             let from = self.env().caller();
             self.transfer_from_to(from, to,value)
         }
 
+        // Aprueba una asignación de gasto de tokens para otra cuenta
         #[ink(message)]
         pub fn approve(&mut self, spender: AccountId, value: Balance) -> Result<()> {
             let owner = self.env().caller();
@@ -107,6 +125,7 @@ mod erc20 {
             Ok(())
         }
 
+        // Transfiere tokens de una cuenta a otra en nombre de la cuenta llamante
         #[ink(message)]
         pub fn transfer_from(
             &mut self,
@@ -122,8 +141,9 @@ mod erc20 {
             self.transfer_from_to(from, to, value)?;
             self.allowances.insert((from, caller), &(allowance - value));
             Ok(())
-        }
+        }   
 
+        // Realiza la transferencia de tokens de una cuenta a otra
         #[ink(message)]
         pub fn transfer_from_to(
             &mut self,
